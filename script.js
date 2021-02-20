1d13ca967da83a152f1b2bb6ef99b1cffa34ed77
Arman Sadeghi <arman1998@gmail.com>
2021-02-20 11:14:24 -0800

    changes

--- script.js
+++ script.js
@@ -4,17 +4,6 @@ function submission() {
     alert("the button was pressed");
 }
 
-<<<<<<< HEAD
-function protect(){
-  var password = "armans";
-  var enteredPass = prompt("enter password to view grades");
-  if (enteredPass == password){
-    alert("correct password, redirecting");
-  } else {
-    alert("incorrect password");
-      location.replace("main_resume.html");
-  }
-=======
 function protect() {
     var password = "armans";
     var enteredpass = prompt("enter password to view grades");
@@ -25,5 +14,4 @@ function protect() {
         alert("incorrect password");
     }
 
->>>>>>> c98c75cb8cc08ef834622c538410d4ee3c8a416d
 }
