63f99c37ebdedd5f1f83025ac09ee575de68ce6a
josephsintum <sjumbam15@gmail.com>
2021-02-20 11:00:29 -0800

    working protect()

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 console.log("hello");
 
-function submission(){
-  alert("the button was pressed");
+function submission() {
+    alert("the button was pressed");
 }
 
-function protect(){
-  var password = "armans";
-  var enteredpass =
-  prompt("enter password to view grades");
-  if (enteredpass == password){
-    alert("correct password, redirecting");
-    return true;
-  } else {
-    alert("incorrect password");
-    return false;
-    //location.reload();
-  }
+function protect() {
+    var password = "armans";
+    var enteredpass = prompt("enter password to view grades");
+    if (enteredpass === password) {
+        alert("correct password, redirecting");
+        location.href = "transcript.html";
+    } else {
+        alert("incorrect password");
+    }
 
 }
