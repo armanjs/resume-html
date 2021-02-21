7230f7d61862fac9b35dab866762f4dddfe99d7c
josephsintum <sjumbam15@gmail.com>
2021-02-21 00:38:03 -0800

    centering content

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 console.log("hello");
 
 function submission() {
-    let option = document.getElementById("ProjectSelection").value;
-    if (option === "project1") {
-        location.href = "project1.html";
-    } else if (option === "project2") {
-        location.href = "project2.html";
-    }
-    //let project = decument.forms["projects"]["project"].value;
-    //location.replace(project);
+  let option = document.getElementById("ProjectSelection").value;
+  if (option === "project1") {
+    location.href = "project1.html";
+  } else if (option === "project2") {
+    location.href = "project2.html";
+  }
+  //let project = decument.forms["projects"]["project"].value;
+  //location.replace(project);
 }
 
 function goBack() {
-    location.href = "resume.html";
+  location.href = "resume.html";
 }
 
 function protect() {
-    var password = "arman";
-    var enteredpass = prompt("enter password to view grades");
-    if (enteredpass === password) {
-        alert("correct password, redirecting");
-        location.href = "transcript.html";
-    } else {
-        alert("incorrect password");
-    }
+  var password = "arman";
+  var enteredpass = prompt("enter password to view grades");
+  if (enteredpass === password) {
+    alert("correct password, redirecting");
+    location.href = "transcript.html";
+  } else {
+    alert("incorrect password");
+  }
 }
