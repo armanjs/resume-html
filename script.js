f8444c5ede44d3f577bfc0ee1ef2b673cf6d2126
Arman Sadeghi <arman1998@gmail.com>
2021-02-20 23:30:49 -0800

    updated script

--- script.js
+++ script.js
@@ -2,12 +2,22 @@ console.log("hello");
 
 function submission() {
     alert("the button was pressed");
-    let project = decument.forms["projects"]["project"].value;
-    location.replace(project);
+    let option = document.getElementById("ProjectSelection").value;
+    if (option === "project1"){
+      window.location.replace("project1.html");
+    } else if (option === "project2"){
+      window.location.replace("project2.html");
+    }
+    //let project = decument.forms["projects"]["project"].value;
+    //location.replace(project);
+}
+
+function goBack(){
+  location.href = "resume.html";
 }
 
 function protect() {
-    var password = "armansadeghi";
+    var password = "arman";
     var enteredpass = prompt("enter password to view grades");
     if (enteredpass === password) {
         alert("correct password, redirecting");
